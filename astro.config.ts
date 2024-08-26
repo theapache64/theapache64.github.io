5cc9d0aff56c6da98fe0caece1bf862ba71eeded
theapache64 <theapache64@gmail.com>
2024-08-26 20:36:41 +0530

    🌟 add GA

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,8 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import partytown from '@astrojs/partytown'
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +17,11 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+  }),
   ],
   markdown: {
     remarkPlugins: [
